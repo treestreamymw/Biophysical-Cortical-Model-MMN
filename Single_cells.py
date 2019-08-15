45d49d53dbb04a90c4f84ca027516fb4543db5d2
Gili karni <gilikarni@dhcp-250-061.ni.tu-berlin.de>
2019-08-15 12:01:25 +0200

    change params 2

--- Single_cells.py
+++ Single_cells.py
@@ -17,8 +17,8 @@ def run_sim (params):
 if __name__ == '__main__':
     ## basic param modification
 
-    grid_search_array = [0.001,0.005,0.01,0.02,0.03,0.04,0.05,0.07]
+    grid_search_array = [0.041, 0.043, 0.045, 0.047, 0.049]
 
 
-    sim_pool_manager = PoolManager(num_workers=10)
+    sim_pool_manager = PoolManager(num_workers=5)
     sim_pool_manager.worker(run_sim, grid_search_array, 999999999)
