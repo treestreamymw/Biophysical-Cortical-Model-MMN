211ba38548613c6cd153b351ba0f24aea64bbf39
Gili karni <gilikarni@dhcp-250-061.ni.tu-berlin.de>
2019-08-15 12:22:46 +0200

    add kca

--- Single_cell_params_optimizing.py
+++ Single_cell_params_optimizing.py
@@ -1,9 +1,20 @@
 import json
 import matplotlib.pyplot as plt
 
-with open('output_files/single_cell_1.json') as json_file:
+with open('output_files/Data_0.045.json') as json_file:
     data = json.load(json_file)
 
-print(data.keys())
-print(data['simData'].keys())
-print(data['simData']['spkt'])
+
+
+bins = {i:0 for i in range(5)}
+
+
+for spike in data['simData']['spkt']:
+    bins[int((spike-200) /2000)]+=1
+
+print(bins)
+
+plt.scatter(bins.keys(),bins.values())
+plt.plot(bins.keys(),bins.values())
+
+plt.show()
--- Single_cells_params.py
+++ Single_cells_params.py
@@ -36,7 +36,7 @@ def set_params(input_rs_threshold):
 
 
 	# ToDo: Change!
-	currents_rs = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
+	currents_rs = [0.0, 0.05, 0.1, 0.15, 0.2]#, 0.25, 0.3]
 	threshold_rs = input_rs_threshold
 
 
