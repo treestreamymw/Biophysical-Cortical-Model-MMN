726d0ab7d4c9762614c7548669f36eb676e0d3db
Gili karni <gilikarni@dhcp-250-078.ni.tu-berlin.de>
2019-09-13 10:34:53 +0200

    add test

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 
 original_params={'duration':10000, 'n_pulses':10, 'n_dev':3, 'size':[1880, 2500, 1880], 'scale':1}
 
+test_params={'duration':1500, 'n_pulses':2, 'n_dev':0, 'size':[360, 2500, 920], 'scale':10}
+
 short_sim_params={'duration':3000, 'n_pulses':3, 'n_dev':1, 'size':[360, 2500, 920], 'scale':10}
 full_sim_params={'duration':10000, 'n_pulses':10, 'n_dev':3,'size':[520, 2500, 920], 'scale':6}
-SIM_PARAMS = {'full':full_sim_params, 'short':short_sim_params, 'orig':original_params}
+
+IM_PARAMS = {'full':full_sim_params, 
+                'short':short_sim_params,
+                'orig':original_params,
+                'test':test_params}
 
 
 
