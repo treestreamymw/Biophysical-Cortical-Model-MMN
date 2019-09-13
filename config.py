b5e328cea1c12d0526585f48aa1e16352126436d
Gili karni <gilikarni@dhcp-250-078.ni.tu-berlin.de>
2019-09-13 10:40:12 +0200

    typo

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ test_params={'duration':1500, 'n_pulses':2, 'n_dev':0, 'size':[360, 2500, 920], 
 short_sim_params={'duration':3000, 'n_pulses':3, 'n_dev':1, 'size':[360, 2500, 920], 'scale':10}
 full_sim_params={'duration':10000, 'n_pulses':10, 'n_dev':3,'size':[520, 2500, 920], 'scale':6}
 
-IM_PARAMS = {'full':full_sim_params, 
+SIM_PARAMS = {'full':full_sim_params,
                 'short':short_sim_params,
                 'orig':original_params,
                 'test':test_params}
