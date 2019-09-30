d0535233706a438fb978458d477017f6168571ed
Gili karni <gilikarni@hey-gorgeous.local>
2019-09-30 11:37:09 +0100

    original guassian pyr4 pyr23 weight to 0.1, guassian to 0.5

--- net_params.py
+++ net_params.py
@@ -211,7 +211,7 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS):
     netParams.connParams['PYR4->PYR23'] = {
         'preConds': {'popLabel': 'PYR4'}, 'postConds': {'popLabel': 'PYR23'},
         'sec': 'basal2b',
-        'probability': '0.5*exp(-dist_2D/(1*40.0))',
+        'probability': '0.5*exp(-dist_2D/(.5*40.0))',
         'weight': 0.1,#0.03,
         'threshold': 10,
         'synMech': 'AMPASTD'}
