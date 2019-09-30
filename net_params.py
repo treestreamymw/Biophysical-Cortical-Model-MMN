1b9fed529d15af44a41b2af8c9b77b1957a70343
Gili karni <gilikarni@hey-gorgeous.local>
2019-09-30 16:46:51 +0100

    pyr4 pyr23 : w=0.1, G=0.1, orig pyr23 pyr23

--- net_params.py
+++ net_params.py
@@ -175,13 +175,13 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS):
     '''
 
     # Layer 2/3 intra-laminar connections
-    '''netParams.connParams['PYR23->PYR23'] = {
+    netParams.connParams['PYR23->PYR23'] = {
         'preConds': {'popLabel': 'PYR23'}, 'postConds': {'popLabel': 'PYR23'},
         'sec':'oblique2b',
         'probability': '0.15*exp(-dist_3D/(4*40.0))',
         'weight': [0.0012,0.0006],
         'threshold': 10,
-        'synMech': ['AMPA','NMDA']}'''
+        'synMech': ['AMPA','NMDA']}
 
     netParams.connParams['PYR23->BASK23'] = {
         'preConds': {'popLabel': 'PYR23'}, 'postConds': {'popLabel': 'BASK23'},
