97f91e472486130b0d90e65e6afaefd0809db5cc
Gili karni <gilikarni@hey-gorgeous.local>
2019-10-21 14:13:27 +0100

    simple_moedl pyr4 pyr 23 g=2 w 0.3 bsak 23 w-0.00015 g=3

--- net_params.py
+++ net_params.py
@@ -274,7 +274,7 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS):
         'preConds': {'popLabel': 'PYR4'}, 'postConds': {'popLabel': 'PYR23'},
         'sec': 'basal2b',
         'probability': '0.5*exp(-dist_2D/(2*40.0))',
-        'weight': 0.03,
+        'weight': 0.3,
         'threshold': 10,
         'synMech': 'AMPASTD'}
 
