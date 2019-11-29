b1875d84b12f77594999d81863324913777537bf
Gili karni <gilikarni@hey-gorgeous.lan>
2019-11-29 17:46:35 +0000

    run 3 4 5

--- run_sim.py
+++ run_sim.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     i=6
     grid_search_array = [
         ['beta_network_single_run_{}'.format(i),
-                SIM_TYPE, TASK, 1, [i]]]
+                SIM_TYPE, TASK, 1, [i]] for i in [3,4,5] ]
 
 
     sim_pool_manager = PoolManager(num_workers=1)
