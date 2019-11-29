70d7f5f7e1b8e1a28b0b83e7be74c9af130fb56d
Gili karni <gilikarni@hey-gorgeous.lan>
2019-11-29 17:49:13 +0000

    run 3 4 5 typo

--- net_params.py
+++ net_params.py
@@ -381,7 +381,7 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
 
     # Saving
     simConfig.saveFolder = 'output_files/'
-    simConfig.filename = 'output_files/''+fig_name  # Set file output name
+    simConfig.filename = 'output_files/{}'.format(fig_name)  # Set file output name
     simConfig.saveFileStep = 1000  # step size in ms to save data to disk
     simConfig.savePickle = False
     simConfig.saveJson = True
