58c683b7e4cdcdcc5dfabd624fa47b734503839e
Gili karni <gilikarni@hey-gorgeous.lan>
2019-12-01 19:21:49 +0000

    change pyr model type // delay the intput by 50 ms // change name

--- run_sim.py
+++ run_sim.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     SIM_TYPE='full' #short
     TASK='flipflop'
     grid_search_array = [
-        ['beta_network_short_stim_{}'.format(i),
+        ['beta_network_short_stim_delay_memory_{}'.format(i),
                 SIM_TYPE, TASK, 1, [i]] for i in [5,6,7] ]
 
 
