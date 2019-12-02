984c5a2353fcd350195f7e4ed2c60e153b095155
Gili karni <gilikarni@hey-gorgeous.lan>
2019-12-02 14:48:54 +0000

    plot 2dnet and shape short stim typo

--- run_sim.py
+++ run_sim.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     ## basic param modification
 
     ## grid_search_array - d1, d2, fig_name
-    SIM_TYPE='short_large_sim_params' #short
+    SIM_TYPE='short_large' #short
     TASK='flipflop'
     grid_search_array = [
         ['beta_network_short_stim_plot_{}'.format(i),
