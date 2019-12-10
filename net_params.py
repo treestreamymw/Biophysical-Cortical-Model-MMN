0c0887d10e2396b57ba594e700aae99f63b6813f
Gili karni <gilikarni@hey-gorgeous.lan>
2019-12-10 18:23:52 +0000

    adding memory

--- net_params.py
+++ net_params.py
@@ -52,13 +52,13 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
                                    'color': 'green'}
 
     ## excitatory cells - memory layer
-    '''netParams.popParams['PYR_memory']={'cellModel': 'PYR_Hay_asym',
-                                    'cellType': 'PYR_asym',
+    netParams.popParams['PYR_memory']={'cellModel': 'PYR_Hay_mem',
+                                    'cellType': 'PYR_mem',
                                     'gridSpacing': 40.0,
                                     'xRange': [20, netParams.sizeX],
                                     'yRange': [1*netParams.sizeY,
                                                 1*netParams.sizeY],
-                                    'color': 'purple'}'''
+                                    'color': 'purple'}
 
     ## inhibitory cells layer 2/3- prediction layer
     netParams.popParams['BASK23']={'cellModel': 'BASK_Vierling',
@@ -79,9 +79,10 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
 
 
     ## PYR cell of layer 4 properties - defined by the four compartment model
-    cellRule=netParams.importCellParams(label='PYR4', conds={'cellType': 'PYR_four',
-                                                     'cellModel': 'PYR_Hay_four'},
-                                          fileName='Cells/fourcompartment.hoc',
+    cellRule=netParams.importCellParams(label='PYR4',
+                                        conds={'cellType': 'PYR_four',
+                                          'cellModel': 'PYR_Hay_four'},
+                                        fileName='Cells/fourcompartment.hoc',
                                   		cellName='fourcompartment')
 
     cellRule['secs']['soma']['vinit']=-80.0
@@ -90,16 +91,18 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
     cellRule['secs']['apic_1']['vinit']=-80.0
 
     ## PYR cell 2/3 properties - defined by the asymetrical stripped model
-    cellRule=netParams.importCellParams(label='PYR23', conds={'cellType': 'PYR_asym',
-                                                       'cellModel': 'PYR_Hay_asym'},
-                                          fileName='Cells/pyr_23_asym_stripped.hoc',
+    cellRule=netParams.importCellParams(label='PYR23',
+                                        conds={'cellType': 'PYR_asym',
+                                             'cellModel': 'PYR_Hay_asym'},
+                                        fileName='Cells/pyr_23_asym_stripped.hoc',
                                   		cellName='pyr_23_asym_stripped')
 
     ## PYR cell of memory layer - defined by the four compartment model
-    '''cellRule=netParams.importCellParams(label='PYR_memory', conds={'cellType': 'PYR_asym',
-                                                       'cellModel': 'PYR_Hay_asym'},
-                                          fileName='Cells/pyr_23_asym_stripped.hoc',
-                                  		cellName='pyr_23_asym_stripped')'''
+    cellRule=netParams.importCellParams(label='PYR_memory',
+                                        conds={'cellType': 'PYR_mem',
+                                            'cellModel': 'PYR_Hay_mem'},
+                                        fileName='Cells/pyr_23_asym_stripped.hoc',
+                                  		cellName='pyr_23_asym_stripped')
 
 
 
@@ -239,7 +242,7 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
         # Internal stimuli - memory trace
 
         # set stimulus name
-        '''int_stim_pop_name='internal_' + \
+        int_stim_pop_name='internal_' + \
             str(internal_pulses_info[t_pulse]['pop_name']) +"_"+ str(t_pulse)
 
         # parametarize stimulus
@@ -259,7 +262,7 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
             'convergence': 1,
             'weight': 0.02,
             'threshold': 10,
-            'synMech': 'AMPA'}'''
+            'synMech': 'AMPA'}
 
 
     ###############################################################################
@@ -339,14 +342,14 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
         'synMech': 'GABA'}'''
 
     ## prediction layer
-    '''netParams.connParams['PYR_memory->PYR_memory']={
+    netParams.connParams['PYR_memory->PYR_memory']={
         'preConds': {'popLabel': 'PYR_memory'},
         'postConds': {'popLabel': 'PYR_memory'},
         'sec':'oblique2b',
         'probability': '0.15*exp(-dist_3D/(4*40.0))',
         'weight': [0.0024,0.00012],
         'threshold': 10,
-        'synMech': ['AMPA','NMDA']}'''
+        'synMech': ['AMPA','NMDA']}
 
     ### inter-laminar connections
     netParams.connParams['PYR4->PYR23']={
@@ -369,13 +372,13 @@ def set_params(fig_name, NET_TYPE, TASK, DEBUG_PARAMS, DEV_LIST):
 
 
 
-    '''netParams.connParams['PYR_memory->BASK23']={
+    netParams.connParams['PYR_memory->BASK23']={
         'preConds': {'popLabel': 'PYR_memory'},
         'postConds': {'popLabel': 'BASK23'},
         'probability': '0.8*exp(-dist_3D/(3*40.0))',
         'weight': 0.00015,
         'threshold': 10,
-        'synMech': 'AMPASTD'}'''
+        'synMech': 'AMPASTD'}
 
 
     ########################################################################
