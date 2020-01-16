db82df7e122eecdc561c2310ea088ed7bf6a2ff9
karnigili <gili.karni.sun@gmail.com>
2020-01-16 17:36:37 +0800

    short stim no int stim to mem layer

--- run_sim.py
+++ run_sim.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     SIM_TYPE='short'#'full' #short
     TASK='flipflop'
     grid_search_array = [
-        ['beta_network_mmn_short_{}'.format(i),
+        ['beta_network_ssa_short_{}'.format(i),
                 SIM_TYPE, TASK, 1, [i]] for i in [2] ]
 
 
