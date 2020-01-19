f2c6d4ea9ef7566805a66e9ed9a563f4540a68b6
karnigili <gili.karni.sun@gmail.com>
2020-01-19 16:12:03 +0800

    fix stim-typo omission 5

--- run_sim.py
+++ run_sim.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
 
     ## grid_search_array - d1, d2, fig_name
     SIM_TYPE='full'#'full' #short
-    TASK='oddball'
+    TASK='omission'
     grid_search_array = [
-        ['beta_4_flipflop_{}'.format(i),
+        ['beta_4_omission_{}'.format(i),
                 SIM_TYPE, TASK, 1, [i]] for i in [5] ]
 
 
