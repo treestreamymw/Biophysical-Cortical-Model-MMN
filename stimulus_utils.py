ebfc1d2fb5fd2a1a9d2e2d0fabacf7104da2c742
karnigili <gili.karni.sun@gmail.com>
2020-01-20 19:22:58 +0800

    debug cascade and many s

--- stimulus_utils.py
+++ stimulus_utils.py
@@ -145,8 +145,8 @@ class Simulation_stimuli_Handler(object):
 
         x_values = self._get_all_available_tones()
         shuffle(x_values)
-        pop_values={'std':{'x_values': [[0,0],[0,0]],
-                                'pulses':[i for i in range(self.n_pulses)]}}
+        pop_values={i:{'x_values': x_values[i],
+                                'pulses':[i] } for i in range(self.n_pulses)}
 
         self.stim_pop_values['external']=pop_values
         self.stim_pop_values['internal']={'std':{'x_values': [[0,0],[0,0]],
@@ -166,7 +166,6 @@ class Simulation_stimuli_Handler(object):
             stimuli_origin='external'
         else:
             stimuli_origin='internal'
-
         for pop in self.stim_pop_values[stimuli_origin]:
 
             if self.stim_pop_values[stimuli_origin][pop] != {}:
@@ -183,14 +182,14 @@ class Simulation_stimuli_Handler(object):
             return self.stimulus_time['internal']
 
 if __name__=="__main__":
-    TASK='many_standards'
+    TASK='cascade'
     s=Simulation_stimuli_Handler(300 ,3 ,40,[2],TASK)
 
     s.set_task_stimuli()
     #print(s.population_values)
     NET_TYPE='short'
 
-    pulses_info=s.get_formatted_pulse(external=False)
+    pulses_info=s.get_formatted_pulse(external=True)
     #pulses_info_ext=s.get_formatted_pulse(external=True)
 
     deviant_pulses_indexes = [2]
@@ -199,14 +198,14 @@ if __name__=="__main__":
     times=s.get_pulse_time(external=False)
 
     for t_pulse in pulses_info.keys():
-
         stim='Stim_' + str(pulses_info[t_pulse]['pop_name']) + str(t_pulse)
 
         netparams[stim] = {'cellModel': 'VecStim',
                    'numCells': 24, 'spkTimes':[0],
                    'pulses':[{'start': t_pulse*1000+times[0],
                        'end': t_pulse*1000.0+times[1], 'rate': 200, 'noise': 1.0}]}
-        print(pulses_info[t_pulse]['values'])
+
         x_pyr, x_bask=pulses_info[t_pulse]['values']
-        print(x_pyr, x_bask)
+        #print(x_pyr, x_bask)
+
     print (netparams)
