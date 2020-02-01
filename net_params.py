55bb4f7c0bfd3a3fa7fd599850ed45d882491522
karnigili <gili.karni.sun@gmail.com>
2020-02-01 13:17:13 +0800

    increase bask 2/3 > pyr 2/3 to 0.8 mem to bask23 .06 pyr pyr 23 0.0024,0.00012 gussian 2

--- net_params.py
+++ net_params.py
@@ -293,7 +293,7 @@ def set_params(fig_name, NET_TYPE, TASK, SEED, DEV_LIST):
         'preConds': {'popLabel': 'PYR4'},
         'postConds': {'popLabel': 'PYR4'},
         'sec': 'apic_1',
-        'probability': '0.15*exp(-dist_3D/(2*40.0))',
+        'probability': '0.15*exp(-dist_3D/(4*40.0))',
         'weight': [0.0012,0.0006],
         'threshold': 10,
         'synMech': ['AMPA','NMDA']}
@@ -329,8 +329,8 @@ def set_params(fig_name, NET_TYPE, TASK, SEED, DEV_LIST):
         'preConds': {'popLabel': 'PYR23'},
         'postConds': {'popLabel': 'PYR23'},
         'sec':'oblique2b',
-        'probability': '0.15*exp(-dist_3D/(4*40.0))',
-        'weight':[0.0020, 0.0001], #[0.0024,0.00012],#[0.0012,0.0006],
+        'probability': '0.15*exp(-dist_3D/(2*40.0))',
+        'weight':[0.0024, 0.00012], #[0.0024,0.00012],#[0.0012,0.0006],
         'threshold': 10,
         'synMech': ['AMPA','NMDA']}
 
